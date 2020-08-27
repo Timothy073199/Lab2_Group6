653d863d2d7206a6d4d3990c4c4a5a9e1ed8ca18
Timothy073199 <70180643+Timothy073199@users.noreply.github.com>
2020-08-27 14:05:55 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-
 function validateForm() {
     var i = document.forms["myevent"]["event"].value;
     var e = document.forms["myevent"]["title"].value;
@@ -9,6 +8,10 @@ function validateForm() {
       alert("Please select event type");
       return false;
     }
+    if (i == "Please choose event type") {
+      alert("Please choose an event type");
+      return false;
+    }
     if (e == "") {
         alert("Please enter event title");
         return false;
@@ -17,17 +20,33 @@ function validateForm() {
         alert("Please enter start date of event");
         return false;
     }
-  
+    if (x >= y) {
+      alert("This date should be greater than the previous date");
+      return false;
+  }
     if (y == "") {
         alert("Please enter end date of event");
         return false;
     }
-  
+    if (y <= x) {
+      alert("This date should be less than the previous date");
+      return false;
+  }
     if (z == "") {
         alert("Please enter the post date of the event");
         return false;
     }
+    if (z <= x) {
+      alert("Posting date should be greater than event date");
+      return false;
+  }
+  if (z >= y) {
+    alert("Posting date should be less than event date");
+    return false;
+}
     else{
     alert("Event saved!")
     }
 }
+
+
